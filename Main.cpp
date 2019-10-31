155c438e45e15a82e5e3f687a1430da03a1670fb
LDanielMedinaC <A01651070@itesm.mx>
2019-10-31 09:38:20 -0600

    something

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,22 @@
+#pragma once
 #include <stdio.h>
 #include <stdlib.h>
 #include "cuadradoMedio.c"
+#include "congruenciaLineal.c"
 int n;
+
+void sumFile(){
+    FILE * cl = fopen("congruenciaLineal.ctxt","r");
+    FILE * cm = fopen("cuadradoMedio.txt","r");
+    FILE * numbers = fopen("numbers.txt", "w");
+    for(int i = 0; i < n; i++){
+        lli a,b,c;
+        fscanf(cl,"%lld", & a);
+        fscanf(cm, "%lld", &b);
+        c = (a + b) % MTEN;
+        fprintf(numbers,"%lld", c); 
+    }
+}
 
 int main(){
 
@@ -11,5 +26,7 @@ int main(){
     fprintf(fp, "%d\n",n);
     fclose(fp);
     cuadradoMedio();
+    congruenciaLineal();
+    sumFile();
     return 0;
 }
---
+++ congruenciaLineal.c
@@ -0,0 +1,39 @@
+#pragma once
+#include <stdio.h>
+
+#define lli unsigned long long int 
+#define SN 10 
+#define MTEN 10000000000
+
+//lli seeds[] = {5915587277, 1500450271, 3267000013, 5754853343, 4093082899, 9576890767, 3628273133, 2860486313, 5463458053, 3367900313};
+
+void CongruenciaLineal(lli seed, lli n, lli a, lli c){
+    FILE * fp;
+    fp = fopen("congruenciaLineal.txt", "w");;
+    fprintf(fp, "%lld\n", seed);
+    for(int i= 1; i < n; i++){
+        seed = (((a * seed)%MTEN) + c) % MTEN;
+        fprintf(fp, "%lld\n", seed);
+    }
+    fclose(fp);
+} 
+
+// lli readN(){
+//     lli n;
+//     FILE *period = fopen("periodNumber.txt","r");
+//     fscanf(period, "%lld", & n);
+//     fclose(period);
+//     return n;
+// }
+
+void congruenciaLineal(){
+    lli seed, n, a, c, m;
+    n = readN();
+    printf("Introduce a seed\n");
+    scanf("%lld", &seed);
+    seed = seeds[seed%SN];
+    printf("Introduce a and c. they must be greater than 0\n");
+    scanf("%lld %lld", &a, &c);
+    
+    CongruenciaLineal(seed, n, seeds[a%SN], seeds[c%SN]);
+}
